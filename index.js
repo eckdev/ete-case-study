97ff9cf70705d4bdcb19c6dcc3fd7721e8bacbe6
Eyüp Can Kayadarçin <eyupcankayadarcin@hotmail.com>
2021-01-30 22:26:24 +0300

    insert client build in node app

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ app.use(bodyParser.json());
 app.use('/api/users', users);
 app.use('/api/pokemons', pokemons);
 
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
